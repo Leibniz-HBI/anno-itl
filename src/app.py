5b695538af9d5bc068022d30d240ba1e68e7f4ab
MattesR <mattes.ruckdeschel@tuhh.de>
2021-10-07 10:50:57 +0200

    add IDs to dataframe, add all pre-parental leave changes

--- src/app.py
+++ src/app.py
@@ -1,4 +1,9 @@
+"""Dash application for annotation of short text usign custom categories and
+giving suggestions for similar text units.
+"""
+
 import dash
+import dash_table
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State, MATCH, ALL
@@ -6,15 +11,18 @@ from dash_html_components.Button import Button
 import datasets
 import html_generators
 import re
+import pandas as pd
 
 app = dash.Dash(__name__)
 
-
-sentences, embeddings = datasets.dataset_from_csv('sentences')
-categories = set(sentence['category'] for sentence in sentences if sentence['category'])
+SIMILARITY_SEARCH_RESULTS = 10
 
 
 
+sentences_df, search_index = datasets.dataset_from_csv('sentences')
+
+# get all categories which are set in the csv except 'None' values
+categories = [cat for cat in sentences_df['category'].unique() if cat]
 
 app.layout = html.Div([
     html.Div(
@@ -27,13 +35,44 @@ app.layout = html.Div([
         className="arg-list-pane",
         children=[
             html.Div(className="arg-list-box", children=[
-                html.Div('Header', className="arg-list-header"),
-                html.Ul(children=[
-                    html.Div(
-                        sentence['sentence'],
-                        id={'type': 'argument-container', 'index': sentence['id']},
-                        className="arg-container") for sentence in sentences]
-            )]
+                html.Div('Argument Input', className="arg-list-header"),
+                dash_table.DataTable(
+                    id='arg-table',
+                    filter_action="native",
+                    columns=[
+                        {'name': 'id', 'id': 'id', 'hidden': True},
+                        {'name': 'Argument', 'id': 'sentence'},
+                        {'name': 'Category', 'id': 'category',  'editable':True, 'presentation': 'dropdown'}
+                    ],
+                    hidden_columns=[
+
+                    ],
+                    dropdown= {
+                        'category': {
+                            'options': [
+                                {'label': cat, 'value': cat} for cat in categories
+                            ]
+                        }
+                    },
+                    data=sentences_df[['id', 'sentence','category']].to_dict('records'),
+                    style_header={
+                        'text_Align': 'center',
+                    },
+                    style_data={
+                        'whiteSpace': 'normal',
+                        'height': 'auto',
+                        'page_size': '50',
+                    },
+                    style_data_conditional=[
+                        {'if': {'column_id': 'sentence'},
+                        'textOverflow': 'ellipsis',
+                        'maxWidth': 0,
+                        'width': '90%',
+                        'textAlign': 'left'
+                        #'cursor': 'pointer'
+                        }
+                    ]
+                )]
             ),
             html.Div('Argument Details', id="argument-detail-box")
         ]
@@ -57,7 +96,43 @@ app.layout = html.Div([
                     ])
                 ]
             ),
-            html.Div("algorithm output here", id='algo-box')
+            html.Div( id='algo-box', children=[
+                html.Div('Similar Arguments', className="algo-box-header"),
+                dash_table.DataTable(
+                    id='algo-table',
+                    columns=[
+                        {'name': 'Argument', 'id': 'sentence'},
+                        {'name': 'Category', 'id': 'category',  'editable':True, 'presentation': 'dropdown'}
+                    ],
+                    dropdown= {
+                        'category': {
+                            'options': [
+                                {'label': cat, 'value': cat} for cat in categories
+                            ]
+                        }
+                    },
+                    data=[],
+                    style_header={
+                        'text_Align': 'center',
+                    },
+                    style_data={
+                        'whiteSpace': 'normal',
+                        'height': 'auto',
+                        'page_size': '50',
+                    },
+                    style_data_conditional=[
+                        {'if': {'column_id': 'sentence'},
+                        'textOverflow': 'ellipsis',
+                        'maxWidth': 0,
+                        'width': '90%',
+                        'textAlign': 'left'
+                        #'cursor': 'pointer'
+                        }
+                    ]
+                )
+
+            ]
+            )
         ]
     )
 ])
@@ -65,20 +140,24 @@ app.layout = html.Div([
 
 @app.callback(
     Output('cat_list', 'children'),
-              Input('submit-cat-button', 'n_clicks'),
-              Input("cat-input", "n_submit"),
-              Input({'type': 'category-remove-btn', 'index': ALL}, 'n_clicks'),
-              State('cat-input', 'value'),
-              State('cat_list', 'children')
+    Output('arg-table', 'dropdown'),
+    Output('algo-table', 'dropdown'),
+        Input('submit-cat-button', 'n_clicks'),
+        Input("cat-input", "n_submit"),
+        Input({'type': 'category-remove-btn', 'index': ALL}, 'n_clicks'),
+    State('cat-input', 'value'),
+    State('cat_list', 'children'),
+    State('arg-table', 'dropdown'),
+    State('algo-table', 'dropdown')
 )
-def add_category(n_clicks, n_submit, remove_click, cat_input, children):
+def add_category(n_clicks, n_submit, remove_click, cat_input, children, arg_dropdown, algo_dropdown):
     """Handle Category input.
     Data from the input box can be submitted with enter and button click. If
     that happens, a new category will be created if it doesn't exist yet.
     If the remove button of a category is presesd, the corresponding list
     element will be deleted.
     """
-    if n_clicks or n_submit:
+    if (n_clicks or n_submit) and cat_input:
         trigger_id = dash.callback_context.triggered[0]['prop_id']
         if trigger_id.startswith('submit-cat-button') or trigger_id.startswith('cat-input'):
             already_there = False
@@ -87,7 +166,7 @@ def add_category(n_clicks, n_submit, remove_click, cat_input, children):
                     already_there = True
             if not already_there:
                 new_cat = html.Li(
-                    children= [
+                    children=[
                         cat_input,
                         html.Button(
                             'remove',
@@ -96,41 +175,85 @@ def add_category(n_clicks, n_submit, remove_click, cat_input, children):
                     id={'type': 'category-item', 'index': len(children)},
                     className="category-container")
                 children.append(new_cat)
+                arg_dropdown['category']['options'] = arg_dropdown['category']['options'] + [{'label': cat_input, 'value': cat_input}]
+                algo_dropdown['category']['options'] = algo_dropdown['category']['options'] + [{'label': cat_input, 'value': cat_input}]
         else:
             button_id = int(re.findall('\d+', trigger_id)[0])
             for child in children:
                 if child['props']['children'][1]['props']['id']['index'] == button_id:
+                    print(child)
                     children.remove(child)
-    return children
+                    try:
+                        arg_dropdown['category']['options'].remove(
+                            {'label': child['props']['children'][0], 'value': child['props']['children'][0]
+                            })
+                    except ValueError:
+                        print(f'no option {cat_input} in dropdown')
+                    try:
+                        algo_dropdown['category']['options'].remove(
+                            {'label': child['props']['children'][0], 'value': child['props']['children'][0]
+                            })
+                    except ValueError:
+                        print(f'no option {cat_input} in dropdown')
+
+    return children, arg_dropdown, algo_dropdown
 
 
 @app.callback(
     Output('argument-detail-box', 'children'),
-    Output('algo-box', 'children'),
-    Input({'type': 'argument-container', 'index': ALL}, 'n_clicks')
+    Output('arg-table', 'data'),
+    Output('algo-table', 'data'),
+        Input('arg-table', 'active_cell'),
+        Input('arg-table', 'dropdown'),
+        Input('arg-table', 'data'),
+        Input('algo-table', 'data'),
+    State('argument-detail-box', 'children')
 )
-def handle_arg_click(n_clicks):
-    """handle click on a list item.
-    When a list item is clicked, the n_clicks property will update, the callback
-    will be triggered and the responsible list element will be stored in
-    `dash.callable_context.triggered`. From that, the index/id of the sentence
-    is known. With that information, the algorithm can be fed and the info box
-    can be changed.
+def handle_input_table_change(active_cell, dropdown, arg_data, algo_data, details_children):
+    """handle changes to input table.
+    There are two scenarios how the argument details or the data for the algo
+    table can change.
+    First, another item is clicked. This changes the output of the detail box as
+    well as the algorithm output. For the algorithm output, the similarity
+    search is conducted and the data of the most similar items is put into the algo-table.
+    Second, a category is deleted and the dropdown options change. When this
+    happens, the algo-table and the arg-table have to clear the dropdown value
+    of all the items that have this category selected.
+    Third, a dropdown item is changed in either datatable (algo or arg). If
+    that's the case, the change must be reflected in the other table as well.
     """
     if not dash.callback_context.triggered[0]['value']:
         raise dash.exceptions.PreventUpdate
+    trigger = dash.callback_context.triggered[0]['prop_id']
+    # first case.
+    if trigger == 'arg-table.active_cell':
+        dff = pd.DataFrame(arg_data)
+        sentence_data = sentences_df.iloc[active_cell['row']]
+        sentence = sentence_data['sentence']
+        details_table = html_generators.create_details_table(sentence_data, header='argument details')
+        similarity_indices = datasets.search_faiss_with_string(
+            sentence,
+            search_index,
+            SIMILARITY_SEARCH_RESULTS
+        )
+        similarity_table_data = dff.iloc[similarity_indices]
 
-
-    sentence_id = int(re.findall('(\d+)',dash.callback_context.triggered[0]['prop_id'])[0])
-    print(dash.callback_context.triggered)
-
-
-    sentence_data = sentences[sentence_id]
-    details_table = html_generators.create_details_table(sentence_data, header='argument details')
-    return details_table, 'Similarity Score list here'
-
-
-
-
+        return details_table, arg_data, similarity_table_data.to_dict('records')
+    # second case.
+    elif trigger == 'arg-table.dropdown':
+        # this means that the dropdown changed, second case!
+        labels = [cat['label'] for cat in dropdown['category']['options']]
+        for index, row in enumerate(arg_data):
+            if row['category'] not in labels:
+                arg_data[index]['category'] = None
+        for index, row in enumerate(algo_data):
+            if row['category'] not in labels:
+                algo_data[index]['category'] = None
+        return details_children, arg_data, algo_data
+    # third case, arg-table data has changed.
+    elif trigger == 'arg-table.data':
+        print (dash.callback_context.triggered)
+    elif trigger == 'algo-table.data':
+        print (dash.callback_context.triggered)
 if __name__ == '__main__':
     app.run_server(debug=True)
--- src/datasets.py
+++ src/datasets.py
@@ -1,37 +1,49 @@
 """ Everything to load data and to make datasets from them. Will be growing as
 input data is more specific
 """
+import os
 import pandas as pd
 from sentence_transformers import SentenceTransformer, util
+from sentence_transformers.models import Normalize
 import numpy as np
+import faiss
 
-model = SentenceTransformer('paraphrase-mpnet-base-v2')
+embedding_model = SentenceTransformer('paraphrase-mpnet-base-v2')
 
+model = SentenceTransformer(modules=[embedding_model, Normalize()])
+
+FAISS_PATH = './faiss_indexes'
 DATA_PATH = './datasets'
 EMBEDDINGS_PATH = './embeddings'
 
 
-def dataset_from_csv(filename, load_embeddings=False):
+def dataset_from_csv(filename):
     """ Creates a dataset from a csv.
     Takes a CSV filename as input (must in be in datasets folder) and creates a
     list of sentences from i, as well as a list of their embeddings.
     The CSV needs to have a column named sentece. For now,
     all is done at once and embeddings are not stored.
+    The function will try to first load an index, then to load embeddings and
+    then will create and store embeddings and a faiss index.
+    The function will also look for a column with IDs, if no column with Ids is
+    in the csv, the ids will be generated from the dataframes' index.
     """
     pd_data = pd.read_csv(f'{DATA_PATH}/{filename}.csv')
-    sentences = pd_data['sentence'].to_list()
-    if load_embeddings:
+    # add blank category column to data without category
+    if 'category' not in pd_data:
+        pd_data['category'] = None
+    if 'id' not in pd_data:
+        pd_data.insert(0, 'id', pd_data.index)
+
+    if os.path.isfile(f'{FAISS_PATH}/{filename}.faiss'):
+        search_index = faiss.read_index(f'{FAISS_PATH}/{filename}.faiss')
+    elif os.path.isfile(f'{EMBEDDINGS_PATH}/{filename}.npy'):
         sentence_embeddings = np.load(f'{EMBEDDINGS_PATH}/{filename}.npy')
     else:
-        sentence_embeddings = model.encode(sentences)
-    if 'categories' in pd_data:
-        sentences = [create_sentence_dict(id, sentence_info[0], category=sentence_info[1])
-                    for id, sentence_info in enumerate(zip(pd_data.sentences, pd_data.categories))]
-    else:
-        sentences = [create_sentence_dict(id, sentence) for id, sentence in enumerate(pd_data['sentence'])]
-
-
-    return sentences, sentence_embeddings
+        sentence_embeddings = model.encode(pd_data['sentence'], convert_to_numpy=True)
+    if not search_index:
+        search_index = create_faiss_index(sentence_embeddings, filename)
+    return pd_data, search_index
 
 
 def store_embeddings(embeddings, filename):
@@ -46,5 +58,26 @@ def create_sentence_dict(id, sentence, category = ''):
     return {'id': id, 'sentence': sentence, 'category': category}
 
 
-def get_similar_args():
-    print('späder päder')
+def create_faiss_index(sentence_embeddings, name):
+    """creates and stores faiss index from sentence embeddings.
+    The normalization is done because it was recommended in the docs when I
+    first used the package (edit: and it still is, on another site in the docs).
+    Args:
+        sentence_embeddings: sentence embeddings to be put into the index.
+        name: name of the index for storing it.
+
+    Returns:
+        The index.
+    """
+    index = faiss.IndexFlatIP(768)
+    index.add(sentence_embeddings)
+    faiss.write_index(index, f'{FAISS_PATH}/{name}.faiss')
+    return index
+
+
+def search_faiss_with_string(text, index, k):
+    """ searches a faiss index with the model and returns the indices of the k
+    most similar entries in the index as a list.
+    """
+    _, I = index.search(model.encode([text]), k = k +1  )
+    return I.tolist()[0][1:]
